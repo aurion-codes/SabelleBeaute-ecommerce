dd124486dbbd544b4fc41eda6cc9840fa8f9032e
aurionhoward <aurionhoward@gmail.com>
2022-01-24 23:23:23 -0500

    component update

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,23 @@ import {useState, useEffect} from 'react'
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("/hello")
+    fetch("/prpducts")
       .then((r) => r.json())
-      .then((data) => setCount(data.count));
+      .then((data) => setProducts(data));
   }, []);
 
   return (
     <BrowserRouter>
     <div className="App">
       <Switch>
-        <Route path="/testing">
-          <h1>Test Route</h1>
+        <Route path="/prducts">
+          <h1>Product List</h1>
         </Route>
         <Route path="/">
-          <h1>Page Count: {count}</h1>
+          <h1>Products</h1>
         </Route>
       </Switch>
     </div>
